f7eb4c675b0364a08e4aee51aacf45ab9c5e1897
Alex Black <wa7@sa34.cx>
2022-01-12 19:09:21 +0300

    Hmmm

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate simplelog;
 
 use simplelog::*;
 
 use std::fs::File;
 
+mod submod;
+
 fn main() {
-    CombinedLogger::init(
-        vec![
-            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
-            WriteLogger::new(LevelFilter::Info, Config::default(), File::create("app.log").unwrap()),
-        ]
-    ).unwrap();
+    let config = ConfigBuilder::new()
+        .set_time_format_str("%Y-%m-%d %H:%M:%S")
+        .build();
+    let config2 = config.clone();
+
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Info,
+            config,
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Error,
+            config2,
+            File::create("app.log").unwrap(),
+        ),
+    ])
+    .unwrap();
 
     trace!("trace");
     debug!("debug");
     info!("info");
     warn!("warn");
     error!("error");
+
+    submod::run();
 }
---
+++ src/submod.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    trace!("sub trace");
+    debug!("sub debug");
+    info!("sub info");
+    warn!("sub warn");
+    error!("sub error");
+}
