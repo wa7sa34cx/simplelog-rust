528f395490aa13441c3b066817b2e5cf84f36f84
Alex Black <wa7@sa34.cx>
2022-01-12 18:08:01 +0300

    First test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[macro_use] extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
+
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            WriteLogger::new(LevelFilter::Info, Config::default(), File::create("app.log").unwrap()),
+        ]
+    ).unwrap();
+
+    trace!("trace");
+    debug!("debug");
+    info!("info");
+    warn!("warn");
+    error!("error");
 }
