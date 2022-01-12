25c09850cb2391d2855596658fa6c3c9d5966d43
Alex Black <wa7@sa34.cx>
2022-01-12 22:24:00 +0300

    Thinking...

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod submod;
 fn main() {
     let config = ConfigBuilder::new()
         .set_time_format_str("%Y-%m-%d %H:%M:%S")
+        .set_target_level(LevelFilter::Error)
         .build();
     let config2 = config.clone();
 
@@ -36,4 +37,6 @@ fn main() {
     error!("error");
 
     submod::run();
+    
+    // let _x = "xyz".parse::<u16>().unwrap();
 }
